952c530792466751f5f74b948f2adc6ea3f7929d
Filip Vušković <filip.vuskovic09@gmail.com>
2020-10-28 23:22:46 +0100

    ADD: filtering + animations

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JSON search app
  A simple search application
+
+ Vanilla JS and some light jQuery for display animations. Loading icon created with pure CSS. Responsiveness ensured by using Bootstrap.
--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,65 @@
-$(document).ready(function(){
+const search = document.getElementById('search');
+const matchList = document.getElementById('result');
+var flag = 0;
 
-    $('#search').keyup(function(){
-        $('#result').html('');
-        
-        var searchField = $('#search').val();
-        var regex = new RegExp(searchField, "i");
+search.addEventListener('input', () => searchFile(search.value));
 
-        $.getJSON('data/data.json', function(data){
-            $.each(data, function(key, value){
-                if(value.title.search(regex) != -1 || value.body.search(regex) != -1){
-                    $('#result').append(`
-                        <div class="result card card-body mb-1">
-                            <h4>${this.title}</h4>
-                            <h6>${this.body}</h6>
-                        </div>
-                    `);
-                }
-            });
-        });
+// Search data.json & filter
+const searchFile = async searchText => {
+    const response = await fetch('data/data.json');
+    const data = await response.json();
+    
+    // Match text to result
+    let matches = data.filter(result => {
+        const regex = new RegExp(`${searchText}`, 'gim');
+        return result.title.match(regex) || result.body.match(regex);
     });
-});
+
+    if(searchText.length === 0 && flag == 1) {
+        matches = [];
+        matchList.innerHTML = '';
+        flag = 0;
+    }
+
+    outputHtml(matches);
+}
+
+// Print html
+const outputHtml = matches => {
+    if(matches.length > 0){
+        const html = matches.map(match => `
+            <div class="card card-body">
+                <h4>${match.title}</h4>
+                <h6>${match.body}</h6>
+            </div>
+        `).join('');
+
+        matchList.innerHTML = html;
+        flag = 1;
+    } else if (flag == 1) {
+        matchList.innerHTML = `
+        <div class="card card-body">
+        <h4>No results found...</h4>
+        </div>
+        `;
+    }
+    showLoading();
+    setTimeout(animateCards, 2000);
+}
+
+//Display animations
+function showLoading() {
+    $('#loading').show(200);
+}
+
+function animateCards(){
+    var i = 1;
+    $('#loading').hide(100);
+    $('.card').each(function(){
+        var self = $(this);
+        setTimeout(function(){
+            self.slideDown(i*300);
+            i++;
+        });    
+    });
+}
